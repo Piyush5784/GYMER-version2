b73db3df827a8d486ceace4a02fd0ad18e9f8175
Piyush Kumar Jha <105655779+Piyush5784@users.noreply.github.com>
2024-02-01 20:06:06 +0530

    Update backend.js

--- Frontend/backend.js
+++ Frontend/backend.js
@@ -1,4 +1,5 @@
-const BackendURL = "https://gymer-version2-ptnksbifp-piyushs-projects-b6f90a7d.vercel.app"
+// const BackendURL = "https://gymer-version2-ptnksbifp-piyushs-projects-b6f90a7d.vercel.app"
+const BackendURL = "https://gymer-version2.vercel.app/"
 
 
 
