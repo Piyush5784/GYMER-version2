c73bdf6975710a82485307885048cfbcee93b7da
Piyush Kumar Jha <105655779+Piyush5784@users.noreply.github.com>
2024-02-02 22:59:53 +0530

    error fixes

--- Frontend/backend.js
+++ Frontend/backend.js
@@ -1,4 +1,4 @@
-const BackendURL = "https://gymer-version2-ptnksbifp-piyushs-projects-b6f90a7d.vercel.app"
+const BackendURL = "https://gymer-backend.onrender.com"
 // const BackendURL = "https://gymer-version2.vercel.app/"
 
 export default BackendURL;
